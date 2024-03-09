cc45f781ece59e3da7e7bc997e3ba0d0ba9f148c
Hamid Sajjadi <hamid.sajjadi@emarketeer.com>
2024-03-09 13:37:21 +0100

    feat(http): add http server and its tests

---
+++ http/server.go
@@ -0,0 +1,75 @@
+package http
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"io"
+	"net/http"
+)
+
+const (
+	keyPathName               = "key"
+	errBadRequestResponse     = "Bad Request"
+	errNotFoundResponse       = "Key Not Found"
+	errInternalServerResponse = "Internal Server Error"
+)
+
+type Cache interface {
+	Set(key string, value string)
+	Get(key string) (string, bool)
+}
+
+func NewServer(logger *zerolog.Logger, cache Cache) http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{key}", get(cache, logger))
+	mux.HandleFunc("POST /{key}", store(cache, logger))
+	var handler http.Handler = mux
+	return handler
+}
+
+func get(cache Cache, logger *zerolog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.PathValue(keyPathName)
+		if key == "" {
+			http.Error(w, errBadRequestResponse, http.StatusBadRequest)
+			return
+		}
+		logger.Trace().Str("key", key).Msg("Received GET key request")
+		value, ok := cache.Get(key)
+		if !ok {
+			logger.Info().Str("key", key).Msg("Cache miss.")
+			http.Error(w, errNotFoundResponse, http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(value))
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to write response")
+		}
+		logger.Info().Str("key", key).Msg("Cache hit.")
+	}
+}
+
+func store(cache Cache, logger *zerolog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.PathValue(keyPathName)
+		if key == "" {
+			http.Error(w, errBadRequestResponse, http.StatusBadRequest)
+			return
+		}
+		logger.Trace().Str("key", key).Msg("Received Post key request")
+		value, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Warn().Err(err).Msg("Failed to read request body")
+			http.Error(w, errBadRequestResponse, http.StatusBadRequest)
+			return
+		}
+		valueStr := string(value)
+		if valueStr == "" {
+			http.Error(w, errBadRequestResponse, http.StatusBadRequest)
+			return
+		}
+		cache.Set(key, valueStr)
+		w.WriteHeader(http.StatusCreated)
+	}
+}
---
+++ http/server_test.go
@@ -0,0 +1,158 @@
+package http
+
+import (
+	"fmt"
+	"github.com/rs/zerolog"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type mockCache struct {
+	Hit      bool
+	GetValue string
+	SetCalls [][]string
+	GetCalls []string
+}
+
+func (m *mockCache) Set(key string, value string) {
+	if m.SetCalls == nil {
+		m.SetCalls = make([][]string, 0, 1)
+	}
+	m.SetCalls = append(m.SetCalls, []string{key, value})
+}
+func (m *mockCache) Get(key string) (string, bool) {
+	if m.GetCalls == nil {
+		m.GetCalls = make([]string, 0, 1)
+	}
+	m.GetCalls = append(m.GetCalls, key)
+	return m.GetValue, m.Hit
+}
+
+func TestServer_Get(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name             string
+		key              string
+		shouldHit        bool
+		expectedStatus   int
+		expectedValue    string
+		expectedGetCalls int
+	}{
+		{
+			name:             "Should return 200 with value cached value",
+			key:              "user-id",
+			shouldHit:        true,
+			expectedStatus:   http.StatusOK,
+			expectedValue:    "cache_value",
+			expectedGetCalls: 1,
+		},
+		{
+			name:             "Should return 404 response because of cache miss",
+			key:              "user-id",
+			shouldHit:        false,
+			expectedStatus:   http.StatusNotFound,
+			expectedGetCalls: 1,
+		},
+		{
+			name:             "Should return 404 because 'GET /' is an invalid route",
+			key:              "",
+			shouldHit:        false,
+			expectedStatus:   http.StatusNotFound,
+			expectedGetCalls: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			cache := &mockCache{
+				Hit:      tt.shouldHit,
+				GetValue: tt.expectedValue,
+			}
+			logger := zerolog.Nop()
+			handler := NewServer(&logger, cache)
+			req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/%s", tt.key), nil)
+			responseRecorder := httptest.NewRecorder()
+			handler.ServeHTTP(responseRecorder, req)
+			if responseRecorder.Code != tt.expectedStatus {
+				t.Fatalf("Expected status code %d, got %d", tt.expectedStatus, responseRecorder.Code)
+			}
+			if tt.expectedValue != "" {
+				if responseRecorder.Body.String() != tt.expectedValue {
+					t.Errorf("Expected response body %s, got %s", tt.expectedValue, responseRecorder.Body.String())
+				}
+			}
+			if len(cache.GetCalls) != tt.expectedGetCalls {
+				t.Errorf(
+					"Expected Get to be called %d times, but was called %d times",
+					tt.expectedStatus,
+					len(cache.GetCalls),
+				)
+			}
+		})
+	}
+}
+
+func TestServer_Post(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name           string
+		key            string
+		body           string
+		expectedStatus int
+	}{
+		{
+			name:           "Should return 201",
+			expectedStatus: http.StatusCreated,
+			key:            "user-id",
+			body:           "user-value",
+		},
+		{
+			name:           "Should return 400 because body is empty",
+			expectedStatus: http.StatusBadRequest,
+			key:            "user-id",
+			body:           "",
+		},
+		{
+			name:           "Should return 404 because 'POST /' is an invalid route",
+			expectedStatus: http.StatusNotFound,
+			key:            "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			cache := &mockCache{}
+			logger := zerolog.Nop()
+			handler := NewServer(&logger, cache)
+			req := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/%s", tt.key), strings.NewReader(tt.body))
+			responseRecorder := httptest.NewRecorder()
+			handler.ServeHTTP(responseRecorder, req)
+			if responseRecorder.Code != tt.expectedStatus {
+				t.Fatalf("Expected status code %d, got %d", tt.expectedStatus, responseRecorder.Code)
+			}
+
+			if tt.expectedStatus == http.StatusCreated {
+				setCall := cache.SetCalls
+				if len(setCall) != 1 {
+					t.Error("Expected cache.Set to be called once")
+				}
+				gotKey := setCall[0][0]
+				gotValue := setCall[0][1]
+				if gotKey != tt.key {
+					t.Errorf(
+						"Expected the cache.Set to be called with (%s, %s), but got (%s, %s)",
+						tt.key,
+						tt.body,
+						gotKey,
+						gotValue,
+					)
+				}
+			}
+
+		})
+	}
+}
